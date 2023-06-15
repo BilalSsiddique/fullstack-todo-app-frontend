53fabdc8eebc4b9b28ed556c42c4a05e89728633
BilalSsiddique <bsiddiqui@students.uit.edu>
2023-06-16 03:44:50 +0500

    udape

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+const port = process.env.PORT || 3001;
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: port,
+  },
   plugins: [react()],
-})
+});
